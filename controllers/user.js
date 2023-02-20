91abb1637c034cac3751856d2e97dd245a0415f3
beingritik <ritikjain98.r@gmail.com>
2023-02-20 09:09:54 +0530

    unstaging

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,13 @@
-const user = require('../models/user');
+const User = require("../models/user");
 
 const login = async function (req, res) {
   console.log("login user entered;");
 };
 
 const registerUser = async function (req, res) {
+  console.log("registered user entered with =",req.body);
+//   const user = new User(re)
   res.send("registered");
-  console.log("registered user entered;");
-  res.redirect("/");
 };
 
 module.exports = {
