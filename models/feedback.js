8308b6e5030542233fdbae81ed9432dc11e00610
beingritik <ritikjain98.r@gmail.com>
2023-02-27 16:50:05 +0530

    last commit today after setting feedback

--- models/feedback.js
+++ models/feedback.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const feedbackSchema = new mongoose.Schema(
   {
     userId: {
@@ -8,77 +7,19 @@ const feedbackSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    aadharnumber: {
-      type: Number,
-      required: [true, "Please provide aadhar number"],
-      match: [
-        /^[(]?[0-9]{3}[)]?[0-9]{3}?[0-9]{4,6}$/,
-        "Please provide valid aadhar number",
-      ],
-      unique: true,
-      minlength: 12,
-      maxlength: 12,
-      trim: true,
-    },
-    dob: {
-      type: Date,
-      required: [true, "Please provide D.O.B"],
-      trim: true,
-    },
-    bloodgroup: {
+    feedback: {
       type: String,
-      required: true,
+      maxlength: 150,
       trim: true,
-      uppercase: true,
-    },
-    address: {
-      type: String,
-      maxlength: 120,
-      trim: true,
-    },
-    dept: {
-      type: String,
-      required: [true, "Department name is required"],
-      uppercase: true,
-      trim: true,
-    },
-    status: {
-      type: String,
-      // enum: ["Enrolled", "Passout", "Verified"],
-      // default: "Enrolled",
-      required: [true, "please provide status value."],
-      trim: true,
-    },
-    rollno: {
-      type: Number,
-      required: [true, "Roll Number is required"],
-      trim: true,
-    },
-    admdate: {
-      type: Date,
-      required: [true, "Please provide admission Date"],
-      trim: true,
-    },
-    phone: {
-      type: Number,
-      match: [
-        /^[(]?[0-9]{3}[)]?[0-9]{3}?[0-9]{4,6}$/,
-        "Please provide valid phone number of 10 digits without any special chars.",
-      ],
-      minlength: 10,
-      maxlength: 10,
-      trim: true,
-      required: true,
-    },
+      required:true
+  },
   },
   { timestamps: true }
 );
 
 feedbackSchema.pre("save", async function (next) {
-  this.status =
-    this.status.charAt(0).toUpperCase() + this.status.slice(1).toLowerCase();
-  this.address =
-    this.address.charAt(0).toUpperCase() + this.address.slice(1).toLowerCase();
+  this.feedback =
+    this.feedback.charAt(0).toUpperCase() + this.feedback.slice(1).toLowerCase();
   next();
 });
 
